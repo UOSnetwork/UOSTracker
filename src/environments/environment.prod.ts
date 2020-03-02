234edbfd1a0943534f4741ff746907d249c3d6ce
Andrew U <10810328+myx0m0p@users.noreply.github.com>
2020-03-02 13:37:37 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  walletUrl: 'https://u.community',
+  walletUrl: 'https://wallet.uos.network',
   votingUrl: 'https://u.community/governance',
   appName: 'U°OS Network Explorer',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'https://history.uos.network:7889',
+  blockchainUrl: 'https://api.uos.network',
   chainId: '61887243d4467f0061b462a63a3e40f3dce917ed7d7cc6a79ad9a1a3be7e5e1e',
   showAds: false,
   tokensUrl: 'https://explorer.uos.network/tokens.json',
